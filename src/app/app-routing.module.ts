1d8c32c9d29ae5c38e3593fe8c1b37d70cf37b5b
Jefferson da Silva <je_jss@hotmail.com>
2020-12-15 00:12:31 -0300

    :sparkles: innnitial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
+import { Bookmark } from './shared/models/bookmark.model';
+const routes: Routes = [
+  {
+    path: '', component: HomePage
+  },
+  {
+    path: 'bookmarks', component: Bookmark
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: '67ebfcef65b3e073386aa685d987fea2',
+};
